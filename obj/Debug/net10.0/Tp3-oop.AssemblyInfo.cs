12b461eb8adc02ad2f00307c7b9804657fb88d36
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-11 14:19:52 +0100

    ajout du gitignore

--- obj/Debug/net10.0/Tp3-oop.AssemblyInfo.cs
+++ obj/Debug/net10.0/Tp3-oop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tp3-oop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5d5ae7b372a8a9f638f0d648532f2a4ce65f2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tp3-oop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tp3-oop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
